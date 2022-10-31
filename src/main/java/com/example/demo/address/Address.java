2493b4edc2ecd158c83f02c3e5870ba573f938f9
AntoniaNicorescu <nicorescu.antonia@gmai.com>
2022-10-31 10:06:27 +0200

    another commit

--- src/main/java/com/example/demo/address/Address.java
+++ src/main/java/com/example/demo/address/Address.java
@@ -1,72 +1,39 @@
 package com.example.demo.address;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 
 
 @Entity // for hibernate
 @Table(name = "address")
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
+    @Getter
+    @Setter
     private Long id;
     @Column(name = "oras")
+    @Getter
+    @Setter
     private String oras;
     @Column(name = "strada")
+    @Getter
+    @Setter
     private String strada;
     @Column(name = "numar")
+    @Getter
+    @Setter
     private Integer numar;
-
-    public Address() {
-    }
-
 
     public Address(String oras, String strada, Integer numar) {
         this.oras = oras;
         this.strada = strada;
         this.numar = numar;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getOras() {
-        return oras;
-    }
-
-    public void setOras(String oras) {
-        this.oras = oras;
-    }
-
-    public String getStrada() {
-        return strada;
-    }
-
-    public void setStrada(String strada) {
-        this.strada = strada;
-    }
-
-    public Integer getNumar() {
-        return numar;
-    }
-
-    public void setNumar(Integer numar) {
-        this.numar = numar;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "  id=" + id +
-                ", oras='" + oras + '\'' +
-                ", strada='" + strada + '\'' +
-                ", numar=" + numar +
-                '}';
     }
 }
